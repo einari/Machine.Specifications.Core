d4cb60c5f68b65a80b573e5a9e966a04649b6763
Einar Ingebrigtsen <einar@dolittle.com>
2016-03-14 08:52:29 +0100

    Started work on the runner

---
+++ Source/Machine.Specifications.Runner.Core/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Machine.Specifications.Runner.Core
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+    
+}
