b29066489c6875ab74118505b437297f1041a06a
Einar Ingebrigtsen <einar@dolittle.com>
2016-03-18 20:44:11 +0100

    The Should extensions are now working as well

---
+++ Source/Machine.Specifications.Should.Core/ComparerStrategies/GenericTypeComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace Machine.Specifications.ComparerStrategies
+{
+    class GenericTypeComparer<T> : IComparerStrategy<T>
+    {
+        public ComparisionResult Compare(T x, T y)
+        {
+            var type = typeof(T);
+
+            if (!type.GetTypeInfo().IsValueType || (type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)))
+            {
+                if (x.IsEqualToDefault())
+                {
+                    return new ComparisionResult(y.IsEqualToDefault() ? 0 : -1);
+                }
+
+                if (y.IsEqualToDefault())
+                {
+                    return new ComparisionResult(-1);
+                }
+            }
+            return new NoResult();
+        }
+    }
+}
--- Source/Runner/Program.cs
+++ Source/Runner/Program.cs
@@ -1,18 +1,82 @@
 using System;
+using System.Reflection;
+using Machine.Specifications.Runner.Impl;
 
 namespace Machine.Specifications.Runner.Core
 {
+    public class Listener : ISpecificationRunListener
+    {
+        public void OnAssemblyEnd(AssemblyInfo assembly)
+        {
+            //Console.WriteLine("Assembly End");
+        }
+
+        public void OnAssemblyStart(AssemblyInfo assembly)
+        {
+            //Console.WriteLine("Assembly Start");
+        }
+
+        public void OnContextEnd(ContextInfo context)
+        {
+        }
+
+        public void OnContextStart(ContextInfo context)
+        {
+            Console.WriteLine($"{context.Name}");
+            
+        }
+
+        public void OnFatalError(ExceptionResult exception)
+        {
+            Console.WriteLine("FATAL");
+        }
+
+        public void OnRunEnd()
+        {
+           // Console.WriteLine("Run End");
+        }
+
+        public void OnRunStart()
+        {
+            //Console.WriteLine("Run Start");
+        }
+
+        public void OnSpecificationEnd(SpecificationInfo specification, Result result)
+        {
+            //Console.WriteLine("Spec End");
+            if( !result.Passed ) Console.WriteLine("    FAILED");
+        }
+
+        public void OnSpecificationStart(SpecificationInfo specification)
+        {
+            Console.WriteLine("  It "+specification.Name);
+        }
+    }
+
+
+    public class when_doing_stuff
+    {
+        Establish context = () => {};
+        
+        
+        Because of = () => {};
+        
+        It should_really_do_things = () => true.ShouldBeTrue();
+        It should_also_do_other_things = () => true.ShouldBeFalse();
+        
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            var listener = new Listener();
+            var options = RunOptions.Default;
+            var specificationRunner = new DefaultRunner(listener, options);
             
-            Console.ReadLine();
-            
-            Console.WriteLine("DONE");
-            
+            var assembly = Assembly.GetEntryAssembly();
+            specificationRunner.RunAssembly(assembly);
+           
         }
     }
-    
 }
